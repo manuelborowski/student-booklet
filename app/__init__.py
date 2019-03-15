9ae2ea4b1be2aadf2d3e79a7c2aa7e1bb7e9113a
manuel borowski <emmanuel.borowski@gmail.com>
2019-03-15 11:33:17 +0100

    introduced nginx

--- app/__init__.py
+++ app/__init__.py
@@ -59,90 +59,49 @@ class MyLogFilter(logging.Filter):
         record.username = current_user.username if current_user and current_user.is_active else 'NONE'
         return True
 
+config_name = os.getenv('FLASK_CONFIG')
+config_name = 'development'
 
-def create_app(config_name):
-    global app
-    global log
+#set up logging
+LOG_FILENAME = os.path.join(sys.path[0], app_config[config_name].STATIC_PATH, 'log/sb-log.txt')
+try:
+    log_level = getattr(logging, app_config[config_name].LOG_LEVEL)
+except:
+    log_level = getattr(logging, 'INFO')
+log.setLevel(log_level)
+log.addFilter(MyLogFilter())
+log_handler = logging.handlers.RotatingFileHandler(LOG_FILENAME, maxBytes=10 * 1024, backupCount=5)
+log_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(username)s - %(message)s')
+log_handler.setFormatter(log_formatter)
+log.addHandler(log_handler)
 
-    #set up logging
-    LOG_FILENAME = os.path.join(sys.path[0], app_config[config_name].STATIC_PATH, 'log/sb-log.txt')
-    try:
-        log_level = getattr(logging, app_config[config_name].LOG_LEVEL)
-    except:
-        log_level = getattr(logging, 'INFO')
-    log.setLevel(log_level)
-    log.addFilter(MyLogFilter())
-    log_handler = logging.handlers.RotatingFileHandler(LOG_FILENAME, maxBytes=10 * 1024, backupCount=5)
-    log_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(username)s - %(message)s')
-    log_handler.setFormatter(log_formatter)
-    log.addHandler(log_handler)
+log.info('start SB')
 
-    log.info('start SB')
+app.config.from_object(app_config[config_name])
+app.config.from_pyfile('config.py')
 
-    app.config.from_object(app_config[config_name])
-    app.config.from_pyfile('config.py')
+Bootstrap(app)
 
-    Bootstrap(app)
+jsglue = JSGlue(app)
+db.app=app  # hack :-(
+db.init_app(app)
 
-    jsglue = JSGlue(app)
-    db.app=app  # hack :-(
-    db.init_app(app)
+app.url_map.converters['int'] = IntegerConverter
 
-    app.url_map.converters['int'] = IntegerConverter
+login_manager.init_app(app)
+login_manager.login_message = 'Je moet aangemeld zijn om deze pagina te zien!'
+login_manager.login_view = 'auth.login'
 
-    if not config.DB_TOOLS:
-        login_manager.init_app(app)
-        login_manager.login_message = 'Je moet aangemeld zijn om deze pagina te zien!'
-        login_manager.login_view = 'auth.login'
+migrate = Migrate(app, db)
 
-        migrate = Migrate(app, db)
+from app import models
 
-        from app import models
+#create_admin(db) # Only once
 
-        #create_admin(db) # Only once
-
-        #flask db migrate
-        #flask db upgrade
-        #uncheck when migrating database
-        #return app
-
-        from .grade import grade as grade_blueprint
-        app.register_blueprint(grade_blueprint)
-
-        from .auth import auth as auth_blueprint
-        app.register_blueprint(auth_blueprint)
-
-        from .settings import settings as settings_blueprint
-        app.register_blueprint(settings_blueprint)
-
-        from .user import user as user_blueprint
-        app.register_blueprint(user_blueprint)
-
-        from .remarks import remarks as remarks_blueprint
-        app.register_blueprint(remarks_blueprint)
-
-        from .reviewed import reviewed as reviewed_blueprint
-        app.register_blueprint(reviewed_blueprint)
-
-        from .documents import init_documents
-        init_documents(app, 'photo')
-
-        @app.errorhandler(403)
-        def forbidden(error):
-            return render_template('errors/403.html', title='Forbidden'), 403
-
-        @app.errorhandler(404)
-        def page_not_found(error):
-            return render_template('errors/404.html', title='Page Not Found'), 404
-
-        @app.errorhandler(500)
-        def internal_server_error(error):
-            return render_template('errors/500.html', title='Server Error'), 500
-
-        @app.route('/500')
-        def error_500():
-            abort(500)
-    return app
+#flask db migrate
+#flask db upgrade
+#uncheck when migrating database
+#return app
 
 #decorator to grant access to admins only
 def admin_required(func):
@@ -162,5 +121,44 @@ def supervisor_required(func):
             abort(403)
         return func(*args, **kwargs)
     return decorated_view
+
+
+
+from .grade import grade as grade_blueprint
+app.register_blueprint(grade_blueprint)
+
+from .auth import auth as auth_blueprint
+app.register_blueprint(auth_blueprint)
+
+from .settings import settings as settings_blueprint
+app.register_blueprint(settings_blueprint)
+
+from .user import user as user_blueprint
+app.register_blueprint(user_blueprint)
+
+from .remarks import remarks as remarks_blueprint
+app.register_blueprint(remarks_blueprint)
+
+from .reviewed import reviewed as reviewed_blueprint
+app.register_blueprint(reviewed_blueprint)
+
+from .documents import init_documents
+init_documents(app, 'photo')
+
+@app.errorhandler(403)
+def forbidden(error):
+    return render_template('errors/403.html', title='Forbidden'), 403
+
+@app.errorhandler(404)
+def page_not_found(error):
+    return render_template('errors/404.html', title='Page Not Found'), 404
+
+@app.errorhandler(500)
+def internal_server_error(error):
+    return render_template('errors/500.html', title='Server Error'), 500
+
+@app.route('/500')
+def error_500():
+    abort(500)
 
 
--- run.py
+++ run.py
@@ -3,11 +3,7 @@
 
 import os
 
-from app import create_app
-
-config_name = os.getenv('FLASK_CONFIG')
-app = create_app(config_name)
-
+from app import app
 
 if __name__ == '__main__':
     import argparse
